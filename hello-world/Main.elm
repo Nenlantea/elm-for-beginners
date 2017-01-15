1b1356d029b51a3b33639c066141e4fab7b5993b
Jamie Curle <jamie@curle.io>
2017-01-15 12:27:32 +0000

    pipe and anon functions in

--- hello-world/Main.elm
+++ hello-world/Main.elm
@@ -3,5 +3,14 @@ module Main exposing (..)
 import Html
 
 
+add a b =
+    a + b
+
+
+result =
+    -- add 1 2 |> add 3
+    add 2 2 |> \a -> a % 2 == 0
+
+
 main =
-    Html.text "Hello World"
+    Html.text (toString result)
